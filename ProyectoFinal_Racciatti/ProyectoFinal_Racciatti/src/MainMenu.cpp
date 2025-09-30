089889e951732d790bc139876b94ee37e6430546
TomasRacciatti <tomasracciatti@gmail.com>
2025-09-30 12:49:27 -0300

    Main menu

--- ProyectoFinal_Racciatti/ProyectoFinal_Racciatti/src/MainMenu.cpp
+++ ProyectoFinal_Racciatti/ProyectoFinal_Racciatti/src/MainMenu.cpp
@@ -4,34 +4,76 @@ MainMenu::MainMenu(ResourceManager& resourceManager, sf::RenderWindow& window)
 	: Scene(window)
 	, text(resourceManager.GetFont("../fonts/MilitaryPoster.ttf"), "PLAY", 48)
 {
+	// BG
 	std::string backgroundPath = "../sprites/backgrounds/MainMenu_bg.png";
 	sf::IntRect backgroundArea({ 0, 0 }, { 1280, 720 });
 	sf::Texture& backgroundTexture = resourceManager.GetTexture(backgroundPath, false, backgroundArea);
 	background = new sf::Sprite(backgroundTexture);
 
+
+	// Shared button data
 	std::string buttonPath = "../sprites/UI/button.png";
 	sf::IntRect buttonArea({ 0, 0 }, { 366, 159 });
-	sf::Texture& startButtonTexture = resourceManager.GetTexture(buttonPath, false, buttonArea);
-	startButton = new sf::Sprite(startButtonTexture);
+	sf::Texture& btnTex = resourceManager.GetTexture(buttonPath, false, buttonArea);
 
-	startButton->setOrigin({ startButton->getTexture().getSize().x * 0.5f,
-							 startButton->getTexture().getSize().y * 0.5f });
-
-	startButton->setScale(sf::Vector2f(0.5f, 0.5f));
-
-	startButton->setPosition({ 1280 * 0.5f, 720 * 0.5f });
-
-	std::string fontPath = "../fonts/MilitaryPoster.ttf";
+	// Font data
+	const std::string fontPath = "../fonts/MilitaryPoster.ttf";
 	sf::Font& font = resourceManager.GetFont(fontPath);
-	buttonText = new sf::Text(font, "PLAY", 48);
+	sf::Color textColor = sf::Color(230, 230, 200);
 
-	/*buttonText->setFont(font);
-	buttonText->setString("PLAY");
-	buttonText->setCharacterSize(48);*/
-	buttonText->setFillColor(sf::Color(230, 230, 200));
-	buttonText->setPosition({ 1280 * 0.5f, 720 * 0.5f });
-	buttonText->setOrigin(buttonText->getLocalBounds().getCenter());
+	// Button sprites
+	startButton = new sf::Sprite(btnTex);
+	optionsButton = new sf::Sprite(btnTex);
+	creditsButton = new sf::Sprite(btnTex);
+	exitButton = new sf::Sprite(btnTex);
 
+	CenterSprite(startButton);
+	CenterSprite(optionsButton);
+	CenterSprite(creditsButton);
+	CenterSprite(exitButton);
+
+	startButton->setScale(sf::Vector2f(buttonScale, buttonScale));
+	optionsButton->setScale(sf::Vector2f(buttonScale, buttonScale));
+	creditsButton->setScale(sf::Vector2f(buttonScale, buttonScale));
+	exitButton->setScale(sf::Vector2f(buttonScale, buttonScale));
+
+	const auto winSize = _window.getSize();
+	const sf::Vector2f center(winSize.x * 0.5f, winSize.y * 0.5f);
+
+	startButton->setPosition({ center.x, center.y + spacingY * 0.f });
+	optionsButton->setPosition({ center.x, center.y + spacingY * 1.f });
+	creditsButton->setPosition({ center.x, center.y + spacingY * 2.f });
+	exitButton->setPosition({ center.x, center.y + spacingY * 3.f });
+
+	// Text
+	playText = new sf::Text(font, "PLAY", 48);
+	optionsText = new sf::Text(font, "OPTIONS", 48);
+	creditsText = new sf::Text(font, "CREDITS", 48);
+	exitText = new sf::Text(font, "EXIT", 48);
+
+
+	playText->setFillColor(textColor);
+	optionsText->setFillColor(textColor);
+	creditsText->setFillColor(textColor);
+	exitText->setFillColor(textColor);
+
+	CenterText(playText);
+	CenterText(optionsText);
+	CenterText(creditsText);
+	CenterText(exitText);
+
+	playText->setPosition(startButton->getPosition());
+	optionsText->setPosition(optionsButton->getPosition());
+	creditsText->setPosition(creditsButton->getPosition());
+	exitText->setPosition(exitButton->getPosition());
+
+	// Title
+	titleText = new sf::Text(font, "CHOPPER HUNTER", 100);
+	titleText->setFillColor(textColor);
+	CenterText(titleText);
+	titleText->setPosition({ winSize.x * 0.5f, 180.f });
+
+	// Music
 	std::string musicPath = "../audio/music/MainMenuTheme.mp3";
 
 	music.openFromFile(musicPath);
@@ -43,9 +85,16 @@ MainMenu::MainMenu(ResourceManager& resourceManager, sf::RenderWindow& window)
 
 MainMenu::~MainMenu()
 {
-	delete buttonText;
+	delete titleText;
+	delete playText;
+	delete optionsText;
+	delete creditsText;
+	delete exitText;
 	delete background;
 	delete startButton;
+	delete optionsButton;
+	delete creditsButton;
+	delete exitButton;
 }
 
 void MainMenu::Input()
@@ -63,9 +112,18 @@ void MainMenu::Update(float dt)
 void MainMenu::Draw()
 {
 	_window.draw(*background);
+
+	_window.draw(*titleText);
+
 	_window.draw(*startButton);
-	_window.draw(*buttonText);
-	//_window->draw(text);
+	_window.draw(*optionsButton);
+	_window.draw(*creditsButton);
+	_window.draw(*exitButton);
+		
+	_window.draw(*playText);
+	_window.draw(*optionsText);
+	_window.draw(*creditsText);
+	_window.draw(*exitText);
 }
 
 void MainMenu::HandleEvents(const sf::Event& ev)
@@ -84,6 +142,44 @@ void MainMenu::HandleEvents(const sf::Event& ev)
 
 				music.stop();
 			}
+
+			if (optionsButton->getGlobalBounds().contains(worldPos))
+			{
+				// Placeholder WIP
+			}
+
+			if (creditsButton->getGlobalBounds().contains(worldPos))
+			{
+				// Placeholder WIP
+			}
+
+			if (exitButton->getGlobalBounds().contains(worldPos))
+			{
+				_window.close();
+				return;
+			}
 		}
 	}
+}
+
+// Helpers
+
+void MainMenu::CenterSprite(sf::Sprite* sprite)
+{
+	if (!sprite) return;
+
+	sprite->setOrigin({ sprite->getTexture().getSize().x * 0.5f,
+						sprite->getTexture().getSize().y * 0.5f });
+
+}
+
+void MainMenu::CenterText(sf::Text* text)
+{
+	if (!text) return;
+
+	sf::FloatRect btn = text->getLocalBounds();
+	sf::Vector2f origin(btn.position.x + btn.size.x * 0.5f,
+						btn.position.y + btn.size.y * 0.5f);
+
+	text->setOrigin(origin);
 }
--- ProyectoFinal_Racciatti/ProyectoFinal_Racciatti/src/MainMenu.h
+++ ProyectoFinal_Racciatti/ProyectoFinal_Racciatti/src/MainMenu.h
@@ -2,19 +2,10 @@
 #include <SFML/Graphics.hpp>
 #include "Scene.h"
 #include "ResourceManager.h"
+#include <vector>
 
 class MainMenu : public Scene
 {
-private:
-	sf::Music music;
-
-	sf::Text* buttonText;
-	sf::Text text;
-
-	sf::Sprite* background;
-
-	sf::Sprite* startButton;
-
 public:
 	MainMenu(ResourceManager& resourceManager, sf::RenderWindow& window);
 	~MainMenu() override;
@@ -25,5 +16,28 @@ public:
 
 	void HandleEvents(const sf::Event& ev) override;
 
+private:
+	sf::Music music;
+
+	sf::Text text;
+
+	sf::Text* titleText;
+	sf::Text* playText;
+	sf::Text* optionsText;
+	sf::Text* creditsText;
+	sf::Text* exitText;
+
+	sf::Sprite* background;
+	sf::Sprite* startButton;
+	sf::Sprite* optionsButton;
+	sf::Sprite* creditsButton;
+	sf::Sprite* exitButton;
+
+	// Helpers
+	static void CenterSprite(sf::Sprite* sprite);
+	static void CenterText(sf::Text* text);
+
+	float buttonScale = 0.5f;
+	float spacingY = 100.f;
 };
 
