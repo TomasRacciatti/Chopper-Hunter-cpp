0f34b45b14a45998c9697872353e9869540d9b98
TomasRacciatti <tomasracciatti@gmail.com>
2025-10-08 20:34:35 -0300

    Header arti

---
+++ ProyectoFinal_Racciatti/ProyectoFinal_Racciatti/src/Artillery.h
@@ -0,0 +1,65 @@
+#pragma once
+#include <memory>
+#include <string>
+#include "Entity.h"
+#include "Level.h"
+#include "ResourceManager.h"
+#include "AudioSettings.h"
+
+class ExplosionEffect;
+
+class Artillery : public Entity
+{
+public:
+    Artillery(const sf::Vector2f& spawnPos,
+        AudioSettings& audio,
+        ResourceManager& resources,
+        const std::string& spritePath,
+        const sf::Vector2i& spriteFrameSize,
+        float visualScale = 1.0f);
+
+    ~Artillery() override;
+
+    void Update(float dt, const Level& lvl) override;
+    void Draw(sf::RenderTarget& rt) const override;
+    void TakeDamage(int dmg) override;
+    sf::FloatRect GetBounds() const override;
+
+    void SetAoETarget(Entity* target) { _aoeTarget = target; }
+
+private:
+    static constexpr int hp = 1;
+
+    // Movimiento puramente vertical
+    float _speedY = 400.f;
+    sf::Vector2f _vel{ 0.f, 0.f };
+
+    // Visual
+    ResourceManager& _resourceManager;
+    sf::Texture* _tex = nullptr; 
+    sf::Sprite _sprite;
+    sf::Vector2i _frameSize{ 64, 64 };
+    float _scale = 1.0f;
+
+    // Explosion parameters
+    bool _exploding = false;
+    int   _damage = 5;
+    float _aoeRadius = 0.f;
+    std::unique_ptr<ExplosionEffect> _explosionFx;
+    Entity* _aoeTarget = nullptr;
+
+    // Explosion anim
+    sf::Vector2i _explosionFrameSize{ 128, 128 };
+    float _explosionFrameTime = 0.05f;
+    int _explosionFrameCount = 12;
+    int _hideSourceFromFrame = 2;
+    float _explosionScale = 1.0f;
+
+    // Audio
+    sf::Sound _whistleSfx;
+    sf::Sound _explosionSfx;
+
+    // Helpers
+    void StartExplosion();
+};
+
